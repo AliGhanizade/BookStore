274f486dc9971d945c346fd7161d5b64b32a6da7
Ali <138152554+AliGhanizade@users.noreply.github.com>
2025-08-30 07:01:46 +0000

    create struct book and slice books

---
+++ model/book.go
@@ -0,0 +1,17 @@
+package model
+
+type Book struct{
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Author string `json:"author"`
+	CreateYear string `json:"create_year"`
+	// CreateTime string `json:"create_time"`
+}
+// {
+// 	"id" : "1",
+// 	"name" : "book1",
+// 	"author" : "ali",
+// 	"create_year" : "2012",
+// }
+var Books []Book
+
