19caaca4c3d7e90936a8b79cb632a96e96083316
AliGhanizade <thealighanizade@gmail.com>
2025-09-01 21:48:24 +0330

    change type ID Make it more readable

--- book_controller/delete.go
+++ book_controller/delete.go
@@ -7,13 +7,13 @@ import (
 )
 
 func (c *BookController) Delete(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "DELETE" {
+	if r.Method != "DELETE" {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
 	var requestBody struct {
-		ID string `json:"id"`
+		ID int `json:"id"`
 	}
 
 	err := json.NewDecoder(r.Body).Decode(&requestBody)
@@ -21,19 +21,23 @@ func (c *BookController) Delete(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "invalid request body ", http.StatusBadRequest)
 		return
 	}
+
 	bookFound := false
 	var updateBooks []model.Book
 
 	for _, book := range model.Books {
 		if book.ID == requestBody.ID {
+			bookFound = true
 			continue
 		}
 		updateBooks = append(updateBooks, book)
 	}
+
 	if !bookFound {
 		http.Error(w, "Book not found ", http.StatusNotFound)
 		return
 	}
+
 	model.Books = updateBooks
 
 	w.Header().Set("Content-Type", "application/json")
