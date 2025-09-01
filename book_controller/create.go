9b0bce02ce05c4fcd72e4199676e4786cbd4514e
AliGhanizade <thealighanizade@gmail.com>
2025-09-01 21:47:50 +0330

    Make it more readable

--- book_controller/create.go
+++ book_controller/create.go
@@ -3,22 +3,22 @@ package bookcontroller
 import (
 	"BookStore/model"
 	"encoding/json"
-	"net/http"
+	"net/http"	
 )
 
 func (c *BookController) Create(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
+	if r.Method != "POST" {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	var newbook model.Book
+	var newBook model.Book
 
-	err := json.NewDecoder(r.Body).Decode(&newbook)
+	err := json.NewDecoder(r.Body).Decode(&newBook)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	model.Create(newbook)
+	model.Create(newBook)
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{"message": "Book Added in slice"})
