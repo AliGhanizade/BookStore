1decd224941dbd1550c43574194ad4625861752a
AliGhanizade <thealighanizade@gmail.com>
2025-09-01 21:49:16 +0330

    Complete Get All Item

--- book_controller/items.go
+++ book_controller/items.go
@@ -6,15 +6,21 @@ import (
 	"net/http"
 )
 
-
-func (c *BookController)GetAllBooks(w http.ResponseWriter , r *http.Request)  {
-	if r.Method == "GET" {
-
-	}
-
-	err := json.NewEncoder(w).Encode(model.Books)
-	if err != nil {
+func (c *BookController) GetAllBooks(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	for _, book := range model.Books {
+		err := json.NewEncoder(w).Encode(book)
+		if err != nil {
+			http.Error(w,err.Error(),http.StatusBadRequest)
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
 	
+
 }
