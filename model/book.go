56378352d9a0f7d8ef2643809378d3173519a777
AliGhanizade <thealighanizade@gmail.com>
2025-09-15 21:13:09 +0330

    add create and seve fucn and delete book from file

--- model/book.go
+++ model/book.go
@@ -1,22 +1,26 @@
 package model
 
+import (
+	cnf "BookStore/config"
+	"encoding/json"
+	"os"
+)
+
 type Book struct {
 	ID         int    `json:"id"`
 	Name       string `json:"name"`
 	Author     string `json:"author"`
 	CreateYear string `json:"create_year"`
-	// CreateTime string `json:"create_time"`
 }
 
-// {
-// 	"id" : "1",
-// 	"name" : "book1",
-// 	"author" : "ali",
-// 	"create_year" : "2012"
-// }
+//	{
+//		"name" : "book1",
+//		"author" : "ali",
+//		"create_year" : "2012"
+//	}
 var Books []Book
 
-func Create(newBook Book) {
+func Create(newBook Book) error {
 	if len(Books) == 0 {
 		newBook.ID = 1
 	} else {
@@ -24,5 +28,47 @@ func Create(newBook Book) {
 		newBook.ID = Books[len(Books)-1].ID + 1
 	}
 
+	file, err := os.OpenFile(cnf.FilePath, os.O_RDWR|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 	Books = append(Books, newBook)
+	jsonBooks, err := json.Marshal(Books)
+	if err != nil {
+		return err
+	}
+	_, err = file.Write(jsonBooks)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Delete(Id int) (bool,error) {
+	found := false
+	for i, book := range Books {
+		if book.ID == Id {
+			Books = append(Books[:i], Books[i+1:]...)
+			found = true
+			break
+		} 
+	}
+	if !found {
+		return found,nil
+	}
+	file, err := os.OpenFile(cnf.FilePath, os.O_RDWR|os.O_TRUNC, 0644)
+	if err != nil {
+		return found,err
+	}
+	defer file.Close()
+	jsonBooks, err := json.Marshal(Books)
+	if err != nil {
+		return found,err
+	}
+	_, err = file.Write(jsonBooks)
+	if err != nil {
+		return found,err
+	}
+	return found,nil
 }
