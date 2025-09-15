1c9c7aaf57c444e92ec3e0e592e234402e256428
AliGhanizade <thealighanizade@gmail.com>
2025-09-15 21:10:47 +0330

    change model for create book

--- book_controller/create.go
+++ book_controller/create.go
@@ -7,10 +7,6 @@ import (
 )
 
 func (c *BookController) Create(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
 	var newBook model.Book
 
 	err := json.NewDecoder(r.Body).Decode(&newBook)
@@ -18,9 +14,13 @@ func (c *BookController) Create(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	model.Create(newBook)
 
-	w.Header().Set("Content-Type", "application/json")
+	err = model.Create(newBook)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
 	json.NewEncoder(w).Encode(map[string]string{"message": "Book Added in slice"})
 
 }
