dbd49a3da2d213b0a34564b72afee2a71cc5d080
AliGhanizade <thealighanizade@gmail.com>
2025-09-15 21:10:14 +0330

    router now better

--- routers/router.go
+++ routers/router.go
@@ -9,10 +9,29 @@ import (
 func Run() {
 	bookC := &bookcontroller.BookController{}
 	r := http.NewServeMux()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 
-	r.HandleFunc("/book/create", bookC.Create)
-	r.HandleFunc("/book/item", bookC.GetAllBooks)
-	r.HandleFunc("/book/delete", bookC.Delete)
+		switch r.Method {
+		case http.MethodGet:
+			query := r.URL.Query()
+			if conditions, ok := query["id"]; ok && len(conditions) > 0 {
+				bookC.GetByID(w, r, conditions[0])
+
+			} else {
+
+				bookC.GetAll(w, r)
+			}
+
+		case http.MethodPost:
+			bookC.Create(w, r)
+		case http.MethodDelete:
+			bookC.Delete(w, r)
+		default:
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	})
 
 	fmt.Println("Server listen on 127.0.0.1:8080")
 	err := http.ListenAndServe(":8080", r)
