33ad81be6151821932a5c22377d2e227ca6395bb
AliGhanizade <thealighanizade@gmail.com>
2025-10-03 20:09:17 +0330

    change BookRepository interface and implement SearchByYear method

--- internal/repository/book_repository.go
+++ internal/repository/book_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"BookStore/internal/domain"
+	"fmt"
 
 	"gorm.io/gorm"
 )
@@ -9,12 +10,13 @@ import (
 type BookRepository interface {
 	Create(book *domain.Book) error
 	FindByID(id uint) (*domain.Book, error)
-	FindByYear(year uint) (*domain.Book, error)
+	FindByTitle(title string) (*domain.Book, error)
+	SearchByYear(year uint) ([]domain.Book, error)
 	SearchByTitle(title string) ([]domain.Book, error)
 	SearchByAuthor(author string) ([]domain.Book, error)
 	Update(book *domain.Book) error
 	Delete(book *domain.Book) error
-	ListAll() ([]domain.Book, error)
+	Paginate(page, pageSize int) ([]domain.Book, error)
 }
 
 type bookRepository struct {
@@ -29,6 +31,14 @@ func (r *bookRepository) Create(book *domain.Book) error {
 	return r.db.Create(book).Error
 }
 
+func (r *bookRepository) FindByTitle(title string) (*domain.Book, error) {
+	var book domain.Book
+	if err := r.db.Where("title = ?", title).First(&book).Error; err != nil {
+		return nil, err
+	}
+	return &book, nil
+}
+
 func (r *bookRepository) FindByID(id uint) (*domain.Book, error) {
 	var book domain.Book
 	if err := r.db.Where("id = ?", id).First(&book).Error; err != nil {
@@ -37,17 +47,17 @@ func (r *bookRepository) FindByID(id uint) (*domain.Book, error) {
 	return &book, nil
 }
 
-func (r *bookRepository) FindByYear(year uint) (*domain.Book, error) {
-	var book domain.Book
-	if err := r.db.Where("year = ?", year).First(&book).Error; err != nil {
+func (r *bookRepository) SearchByYear(year uint) ([]domain.Book, error) {
+	var books []domain.Book
+	if err := r.db.Where("year LIKE ?", fmt.Sprintf("%%%d%%", year)).Find(&books).Error; err != nil {
 		return nil, err
 	}
-	return &book, nil
+	return books, nil
 }
 
 func (r *bookRepository) SearchByTitle(title string) ([]domain.Book, error) {
 	var books []domain.Book
-	if err := r.db.Where("title LIKE ?", title).Find(&books).Error; err != nil {
+	if err := r.db.Where("title LIKE ?", "%"+title+"%").Find(&books).Error; err != nil {
 		return nil, err
 	}
 	return books, nil
@@ -55,11 +65,12 @@ func (r *bookRepository) SearchByTitle(title string) ([]domain.Book, error) {
 
 func (r *bookRepository) SearchByAuthor(author string) ([]domain.Book, error) {
 	var books []domain.Book
-	if err := r.db.Where("author LIKE ?", author).Find(&books).Error; err != nil {
+	if err := r.db.Where("author LIKE ?", "%"+author+"%").Find(&books).Error; err != nil {
 		return nil, err
 	}
 	return books, nil
 }
+
 
 func (r *bookRepository) Update(book *domain.Book) error {
 	if err := r.db.Save(&book).Error; err != nil {
@@ -75,9 +86,9 @@ func (r *bookRepository) Delete(book *domain.Book) error {
 	return nil
 }
 
-func (r *bookRepository) ListAll() ([]domain.Book, error) {
+func (r *bookRepository) Paginate(page, pageSize int) ([]domain.Book, error) {
 	var books []domain.Book
-	if err := r.db.Find(&books).Error; err != nil {
+	if err := r.db.Offset((page - 1) * pageSize).Limit(pageSize).Find(&books).Error; err != nil {
 		return nil, err
 	}
 	return books, nil
