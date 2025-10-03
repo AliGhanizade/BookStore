4d5b865a8b6907b96b4f5953f35f64b4ac646f03
AliGhanizade <thealighanizade@gmail.com>
2025-10-03 20:09:53 +0330

    update Title field constraint to use uniqueIndex for better indexing

--- internal/domain/book.go
+++ internal/domain/book.go
@@ -8,7 +8,7 @@ import (
 
 type Book struct {
 	ID        uint   `gorm:"primaryKey"`
-	Title     string `gorm:"size:100,unique"`
+	Title     string `gorm:"uniqueIndex"`
 	Author    string `gorm:"size:100"`
 	Status    string
 	Year      uint
